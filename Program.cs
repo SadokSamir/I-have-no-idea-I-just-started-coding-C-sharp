f2b0a8d8148713327e65125959710fef9a9817a4
SadokSamir <samir2lmh@gmail.com>
2024-09-28 21:51:19 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualBasic;
+using System;
+class Program
+{
+    static void Main()
+    {
+        //Change The appearamnce
+
+        Console.Title = "The First";
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WindowHeight = 40;
+
+        // Get A conversation Going 
+
+
+        Console.WriteLine("Hello, What is your Name?");
+      Console.ReadLine();
+
+        Console.ForegroundColor= ConsoleColor.Green;
+
+        Console.WriteLine("My Name is Connor, I'm The Android Sent By Cyber Life.");
+        Console.ReadKey();
+
+    }
+}
